4cd9c352fe2a308f8ab72c6a1f4669bf2f229997
Curtis Netterville <curtis@netterville.com>
2025-09-17 12:30:58 -0500

    Initial commit for checkpoints

---
+++ UPS Monitoring/UPS_MonitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  UPS_MonitoringApp.swift
+//  UPS Monitoring
+//
+//  Created by Curtis Netterville on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct UPS_MonitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
